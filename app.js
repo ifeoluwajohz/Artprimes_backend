efad742a87b9718151f08d727e82a600b87bffce
Ifeoluwa joshua <ifeoluwajohz@gmail.com>
2024-08-15 02:56:22 -0700

    make cors receive request from everywhere

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://artprimes.vercel.app'],
+    origin: '*',
     credentials: true
 }));
 
